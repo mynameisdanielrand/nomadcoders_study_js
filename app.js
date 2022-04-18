af8ac1c6ba74891003542ef12e4269c5daa3dfe5
mynameisdanielrand <dbfxodbfxo@gmail.com>
2022-04-18 22:22:12 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //boolean = true or false
 
 //null : 아무것도없음 . 그 변수에 아무것도 없다.
+//null은 자바스크립트에게 여기엔 값이 없다라는것을 알려줄 때 사용.
 
 const amIFat = null;
-console.log(amIFat);
+let something;
+console.log(something, amIFat);
