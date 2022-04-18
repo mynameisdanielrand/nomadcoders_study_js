efbb1c9b6bfab0a186f75576b629d47e5c782dbd
mynameisdanielrand <dbfxodbfxo@gmail.com>
2022-04-18 22:13:17 +0900

    add

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const a = 5;
-const b = 2;
-const myName = 'nico';
+let a = 5;
+let b = 2;
+let myName = 'nico';
+
+// const 는 상수, const 값을 바꿀 수 없음.
+// let은 새로운 것을 생성할 때 사용 let 값을 바꿀 수 있음.
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log('hello ' + myName);
+
+myName = 'nicolas';
+
+console.log('your new name is ' + myName);
+
+// const는 항상 쓰고, let은 가끔 쓰고, var는 절대 쓰지 말것.
