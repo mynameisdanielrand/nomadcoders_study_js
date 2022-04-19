cc64cfa0b8f19c4949dac1c12022c203ffcb8c30
mynameisdanielrand <dbfxodbfxo@gmail.com>
2022-04-19 21:03:32 +0900

    add

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ const mon = 'mon';
 const tue = 'tue';
 const wed = 'wed';
 const thu = 'thu';
+const fri = 'fri';
+const sat = 'sat';
+const sun = 'sun';
+
+const daysOfWeek = mon + tue + wed + thu + fri + sat + sun;
+
+console.log(daysOfWeek);
