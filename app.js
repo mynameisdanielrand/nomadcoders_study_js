80dde5db8d403b26cfec11e7240c884be81c58c3
mynameisdanielrand <dbfxodbfxo@gmail.com>
2022-04-19 21:01:34 +0900

    add

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-//boolean = true or false
-
-//null : 아무것도없음 . 그 변수에 아무것도 없다.
-//null은 자바스크립트에게 여기엔 값이 없다라는것을 알려줄 때 사용.
-
-const amIFat = null;
-let something;
-console.log(something, amIFat);
+const mon = 'mon';
+const tue = 'tue';
+const wed = 'wed';
+const thu = 'thu';
