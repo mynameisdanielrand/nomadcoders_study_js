f45987cfe5a0feacc4609e2bc0cd79c1265b9742
mynameisdanielrand <dbfxodbfxo@gmail.com>
2022-04-20 18:41:51 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-const mon = 'mon';
-const tue = 'tue';
-const wed = 'wed';
-const thu = 'thu';
-const fri = 'fri';
-const sat = 'sat';
-const sun = 'sun';
-
 //array
-const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+const daysOfWeek = ['mon', 'tue', ' wed', 'thu', 'fri', 'sat'];
 
-const nonsense = [1, 2, 'hello', false, null, true, undefined, 'nico'];
-
-console.log(daysOfWeek, nonsense);
+//array를 만드는 방법.
+console.log(daysOfWeek[4]);
