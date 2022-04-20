bad7632c9cc9bb57b31a473e2f1e4aa72028140e
mynameisdanielrand <dbfxodbfxo@gmail.com>
2022-04-20 18:45:23 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ const daysOfWeek = ['mon', 'tue', ' wed', 'thu', 'fri', 'sat'];
 
 //컴퓨터는 1부터가아닌 0부터 숫자를 센다.
 //array를 만드는 방법.
-console.log(daysOfWeek[4]);
+console.log(daysOfWeek);
+
+daysOfWeek.push('sun');
+console.log(daysOfWeek);
