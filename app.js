ea591fd0d123cf3dc34378fb9fc342a69b973305
mynameisdanielrand <dbfxodbfxo@gmail.com>
2022-04-21 23:10:37 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
-//array
-const daysOfWeek = ['mon', 'tue', ' wed', 'thu', 'fri', 'sat'];
+const toBuy = ['potato', 'tomato', 'pizza'];
+toBuy.push('kimbab');
 
-//컴퓨터는 1부터가아닌 0부터 숫자를 센다.
-//array를 만드는 방법.
-console.log(daysOfWeek);
-//array로 부터 아이템 받아오기
-
-//array안에 요일 더 추가하기
-daysOfWeek.push('sun');
-console.log(daysOfWeek);
-
-//array의 목적은 , 하나의 variable 안에 데이터의 list를 가지는 것.
+console.log(toBuy[3]);
