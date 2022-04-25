f14bb76bd7826417a307a4ea0c67de451c70b3f2
mynameisdanielrand <dbfxodbfxo@gmail.com>
2022-04-25 20:03:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const player = {
-    name: 'nico',
+    name: 'daniel',
     points: 10,
     fat: true,
 };
 
 console.log(player);
-player.lastName = 'potato';
+player.lastName = 'rand';
 player.points = 15;
 console.log(player);
